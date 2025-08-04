781f5f3558acee97708205f97259dd7f78883336
Sahin-Halil <79713095+Sahin-Halil@users.noreply.github.com>
2025-08-04 05:20:28 +0100

    Revert "Chip 8 maintain code base"

--- Memory.h
+++ Memory.h
@@ -3,14 +3,12 @@
 
 class Memory {
     private:
-		// Arrays for holding ROM contents
         std::vector<uint8_t> workingMemory;
         std::vector<uint8_t> fonts;
-
     public:
-        Memory(); // Initialises array for holding emulation contents
-        void setMemory(const std::vector<uint8_t>& gameData); // Adds ROM contents 
-        uint8_t getMemory(size_t i); // gets content of a memory address
-        void updateMemory(size_t index, uint8_t data); // Sets content of a memory adress 
-        void printMemory(); // Prints Fonts and ROM contents 
+        Memory();
+        void setMemory(const std::vector<uint8_t>& gameData);
+        uint8_t getMemory(size_t i);
+        void updateMemory(size_t index, uint8_t data);
+        void printMemory();
 };
