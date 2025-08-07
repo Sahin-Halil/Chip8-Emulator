a01ae4226418d90a7cc65b439bdc4dad02eeb1c0
“Sahin” <sahinhalil1311@gmail.com>
2025-08-07 18:59:22 +0100

    Reapply "Chip 8 maintain code base"
    This reverts commit 781f5f3558acee97708205f97259dd7f78883336.

--- CPUTileMapData.h
+++ CPUTileMapData.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <vector>
 
-struct CPUTileMapData
-{
+struct CPUTileMapData {
+	// Shared Attributes
 	private:
 		bool quit;
 		std::vector<uint8_t> V;
 
 	public:
-		CPUTileMapData();
+		CPUTileMapData(); // Initialises shared attributes
+
+		// Getter and setter for quit attribute
 		bool getExitStatus();
 		void setExitStatus(bool quitStatus);
+
+		// Getter and setter for 16 variable registers
 		uint8_t getVRegister(std::size_t vRegister);
 		void setVRegister(std::size_t vRegister, uint8_t registerData);
 };
--- loadROM.cpp
+++ loadROM.cpp
@@ -3,25 +3,31 @@
 #include <vector>
 #include "loadROM.h"
 
+// Load contents of ROM into array
 std::vector<uint8_t> loadROM() {
-    std::vector<uint8_t> gameData = {};
+    std::vector<uint8_t> gameData = {}; // Initialised array to store ROM contents
     
+	// Open ROM as read only binary file
     std::ifstream chip8Game;
     chip8Game.open("Game-Files/bc_test.ch8", std::ios::out | std::ios::binary);
 
+	// Add contents to array if ROM exists
     if (chip8Game.is_open()) {
-        std::cout << "Success: chip8 file was opened" << "\n";
-        uint8_t data = chip8Game.get();
-        while (chip8Game.good()) {
+        // std::cout << "Success: chip8 file was opened" << "\n";
+        uint8_t data = chip8Game.get(); // Get current binary value
+        
+		// Loop through the ROM and add each binary value to array
+		while (chip8Game.good()) {
             //std::cout << +data << "\n";
             gameData.push_back(data);
-            data = chip8Game.get();
+            data = chip8Game.get(); // Get next binary value (if any)
         }
     }
     else {
-        std::cout << "Error: chip8 file could not be opened";
+        std::cout << "Error: chip8 file could not be opened"; // Error message incase ROM was not opened
     }
 
+	// Close ROM when done accessing its contents (even if it didn't exist to save space)
     chip8Game.close();
 
     return gameData;
